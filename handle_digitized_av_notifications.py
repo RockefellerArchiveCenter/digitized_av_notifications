34fa4b294e38d47e4b611c446ffee853e55592cb
Hillel Arnold <hillel.arnold@gmail.com>
2023-03-31 15:08:19 -0500

    rename

--- handle_digitized_av_notifications.py
+++ handle_digitized_av_notifications.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 
-import urllib3 
 import json
 import logging
 import os
 
-http = urllib3.PoolManager() 
+import urllib3
+
+http = urllib3.PoolManager()
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
@@ -17,23 +18,23 @@ def parse_failure_attributes(attributes):
     color_name = '#ff0000'
     status = attributes['outcome']['Value']
     return message, status, color_name
-    
+
 
 def send_teams_message(title, message, status, color_name, url):
     """Delivers message to Teams channel endpoint."""
     notification = {
-            "@type": "MessageCard",
-            "@context": "http://schema.org/extensions",
-            "themeColor": color_name,
-            "summary": message,
-            "sections": [{
-                "activityTitle": title,
-                "text": message,
-                "facts": [{
+        "@type": "MessageCard",
+        "@context": "http://schema.org/extensions",
+        "themeColor": color_name,
+        "summary": message,
+        "sections": [{
+            "activityTitle": title,
+            "text": message,
+            "facts": [{
                     "name": "Status",
                     "value": status
-                }]
-            }]}
+            }]
+        }]}
     encoded_msg = json.dumps(notification).encode('utf-8')
     response = http.request('POST', url, body=encoded_msg)
     logger.info('Status Code: {}'.format(response.status))
@@ -44,11 +45,16 @@ def lambda_handler(event, context):
     """Main handler for function."""
 
     attributes = event['Records'][0]['Sns']['MessageAttributes']
-    
+
     if attributes['outcome']['Value'] == 'FAILURE':
         logger.info("Failure message received.")
         title = event['Records'][0]['Sns']['Message']
-        message, status, color_name = parse_failure_attributes(attributes)    
-        send_teams_message(title, message, status, color_name, os.environ.get('TEAMS_URL'))    
+        message, status, color_name = parse_failure_attributes(attributes)
+        send_teams_message(
+            title,
+            message,
+            status,
+            color_name,
+            os.environ.get('TEAMS_URL'))
     else:
         logger.info("No status worth notifying.")
