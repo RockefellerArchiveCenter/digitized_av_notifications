ea891caba9487f99f711c2cf73aa2d0bd41ab1f6
Hillel Arnold <hillel.arnold@gmail.com>
2023-04-11 12:58:30 -0400

    restructure for better build

--- tests/test_handle_digitized_av_notifications.py
+++ tests/test_handle_digitized_av_notifications.py
@@ -4,12 +4,12 @@ import json
 from pathlib import Path
 from unittest.mock import patch
 
-from handle_digitized_av_notifications import lambda_handler
+from src.handle_digitized_av_notifications import lambda_handler
 
 # TODO set environment variable before tests and unset afterwards
 
 
-@patch('handle_digitized_av_notifications.send_teams_message')
+@patch('src.handle_digitized_av_notifications.send_teams_message')
 def test_success_notification(mock_teams):
     with open(Path('fixtures', 'success_message.json'), 'r') as jf:
         message = json.load(jf)
@@ -17,7 +17,7 @@ def test_success_notification(mock_teams):
         assert mock_teams.call_count == 0
 
 
-@patch('handle_digitized_av_notifications.send_teams_message')
+@patch('src.handle_digitized_av_notifications.send_teams_message')
 def test_failure_notification(mock_teams):
     with open(Path('fixtures', 'failure_message.json'), 'r') as jf:
         message = json.load(jf)
